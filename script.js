2cac21410f71c4f13a362f8e285b8386d1d99307
Saiban Hussein <shhussein@m.marywood.edu>
2022-02-10 21:14:21 -0600

    add javascrip file

---
+++ script.js
@@ -0,0 +1,6 @@
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+const searchBar = document.querySelector("#search")
+let users = [];
+searchInput.value = null;
