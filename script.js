845885453d202cedd14be2ab4158a1440d043f35
Saiban Hussein <shhussein@m.marywood.edu>
2022-02-10 21:16:50 -0600

    add search to javascrip file

--- script.js
+++ script.js
@@ -7,4 +7,22 @@ searchInput.value = null;
 fetch("https://restcountries.com/v3.1/all")
 .then(res => res.json())
 .then(data => {
-  
+
+  users = data.map(user => {
+    const card = userCardTemplate.content.cloneNode(true).children[0];
+    const header = card.querySelector("[data-header]")
+    // const body   = card.querySelector("[data-body]")
+    const flag   = card.querySelector("#flag-container img").src = user.flags.png;
+    // const form   =card.querySelector("#form");
+    const button = card.querySelector("#button");
+    
+
+    // form.addEventListener('submit', function(event){
+    // event.preventDefault()
+    
+    // const file =user.flags.png.files[0];
+     
+    // });
+
+      header.textContent = user.name.common
+        
